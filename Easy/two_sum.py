9f461e707c1e71c319462e4ed92e95c14e3fac3a
chikwanda <chikwanda.chisha.26@dartmouth.edu>
2024-06-27 07:59:57 -0400

    Added centre_of_graph

--- Easy/two_sum.py
+++ Easy/two_sum.py
@@ -1,13 +1,12 @@
 class Solution:
     def twoSum(self, nums:list[int], target:int) -> list[int]:
-        dic = {} #initializing dictionary
-        
+        dic = {}  # initializing dictionary
+
         for i in range(len(nums)):
-            dic[target - nums[i]] = i   #mapping difference to index
-            
-        for j in range(len(nums)):
-            if nums[j] in dic and j != dic[nums[j]]: #check if j != i to avoid repetition
-                return[dic[nums[j]], j]  #return i and j
-        
+            complement = target - nums[i]
+            if complement in dic:
+                return [dic[complement], i]  # return indices if complement is found
+            dic[nums[i]] = i  # map the current number to its index
+
         return []
-        
+            
