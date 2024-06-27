d4fefd0ee21b63137c985dd3eaba298cef0a264d
chikwanda <chikwanda.chisha.26@dartmouth.edu>
2024-06-26 21:05:57 -0400

    Add Max profit and valid abbr

--- Easy/valid_anagram.py
+++ Easy/valid_anagram.py
@@ -1,18 +1,16 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        s_dict = {}
-        t_dict = {}
+        dict_s = {}
+        dict_t = {}
 
-        for i in s:
-            if i in s_dict:
-                s_dict[i] += 1
-            else:
-                s_dict[i] = 1
+        for i in range(len(s)):
+            if s[i] in dict_s:
+                dict_s[s[i]] += 1
+            elif s[i] not in dict_s:
+                dict_s[s[i]] = 1
+            if t[i] in dict_t:
+                dict_t[t[i]] += 1
+            elif t[i] not in dict_t:
+                dict_t[t[i]] = 1
         
-        for j in t:
-            if j in t_dict:
-                t_dict[j] += 1
-            else:
-                t_dict[j] = 1
-        
-        return s_dict == t_dict
+        return dict_s == dict_t
---
+++ Easy/valid_parenthesis.py
@@ -0,0 +1,24 @@
+class Solution:
+    def isValid(self, s:str) -> bool:
+        open_brac_map = {'[':']', '{':'}', '(':')'} # create hashmaps to map a bracket to its complementary bracket
+        closed_brac_map = {']':'[', '}':'{', ')':'('}
+        open_stack = [] # create two stacks to pop off from
+        closed_stack = []
+        
+        for c in s:
+            if c in open_stack: # add to open_stack if the bracket is an open one
+                open_stack.append(c)
+            elif c in closed_stack: # add to closed_stack if the bracket is a closec one
+                closed_stack.append(c)
+            else:
+                return False # return False if the character is not a bracket
+        
+        if len(open_stack) != len(closed_stack): # if the two stacks are not the same length, return False
+            return False
+        
+        for i in range(len(open_stack), -1): # loop through open_stack (or closed_stack) starting with the last index
+            if open_brac_map[open_stack.pop(i)] != closed_stack.pop(i): # if the last item in the open_stack is not complementary to the last in the closed_stack, it's not valid
+                return False
+        
+        return True
+        
