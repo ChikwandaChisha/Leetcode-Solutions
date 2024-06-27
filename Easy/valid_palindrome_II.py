824ae3df4526a092cc1344bfd6b2e0ea6b637cbd
chikwanda <chikwanda.chisha.26@dartmouth.edu>
2024-06-27 07:15:46 -0400

    Modified valid_palindrome_II and valid_parenthesis

--- Easy/valid_palindrome_II.py
+++ Easy/valid_palindrome_II.py
@@ -1,23 +1,26 @@
 class Solution:
     def validPalindrome(self, s:str) -> bool:
+        def substringPalindrome(sub:str) -> bool: # create a helper function to check if the substring is a palindrome
+            sub_l = 0
+            sub_r = len(sub) - 1
+            while sub_l < sub_r:
+                if sub[sub_l] == sub[sub_r]:
+                    sub_l += 1
+                    sub_r -= 1
+                else:
+                    return False
+            return True
+
         l = 0
         r = len(s) - 1
-        del_count = 0
         
         while l < r:
-            if del_count > 1:
-                return False
-            if s[l] == s[r]:
+            if s[l] == s[r]: # update l and r if the two characters are equal
                 l += 1
                 r -= 1
-                continue
             else:
-                if s[l+1] == s[r]:
-                    l += 1
-                    count += 1
-                elif s[l] == s[r-1]:
-                    r -= 1
-                    count += 1
-        
+                return substringPalindrome(s[l:r]) or substringPalindrome(s[l+1:r+1])
         return True
+    
+
         
--- Easy/valid_parenthesis.py
+++ Easy/valid_parenthesis.py
@@ -1,43 +1,19 @@
 class Solution:
     def isValid(self, s:str) -> bool:
-        open_brac_map = {'[':']', '{':'}', '(':')'} # create hashmaps to map a bracket to its complementary bracket
-        closed_brac_map = {']':'[', '}':'{', ')':'('}
-        open_stack = [] # create two stacks to pop off from
-        closed_stack = []
-        
-        for c in s:
-            if c in open_stack: # add to open_stack if the bracket is an open one
-                open_stack.append(c)
-            elif c in closed_stack: # add to closed_stack if the bracket is a closec one
-                closed_stack.append(c)
-            else:
-                return False # return False if the character is not a bracket
-        
-        if len(open_stack) != len(closed_stack): # if the two stacks are not the same length, return False
+        if len(s) % 2 != 0 : # check if string-length is odd or even
             return False
-        
-        for i in range(len(open_stack), -1): # loop through open_stack (or closed_stack) starting with the last index
-            if open_brac_map[open_stack.pop(i)] != closed_stack.pop(i): # if the last item in the open_stack is not complementary to the last in the closed_stack, it's not valid
-                return False
-        
-        return True
+        brackets = {"}":"{", "]":"[", ")":"("}
+        stack = []
+        for i in s:
+            if i in brackets: # if i is a closed bracket
+                if stack and stack[-1] == brackets[i]: # check if stack is empty and if the last elemnt in the stack is complementary with thw current closed bracket
+                    stack.pop() # pop out the last element
+                else:
+                    return False # return false if the two brackets are not compartible
+            else:
+                stack.append(i) # add to stack if it's an open bracket
     
-    
-        # if len(s) % 2 != 0 :
-        #     return False
-        # brackets = {"}":"{", "]":"[", ")":"("}
-        # stack = []
-        # for i in s:
-        #     if i in brackets:
-        #         if stack and stack[-1] == brackets[i]:
-        #             stack.pop()
-        #         else:
-        #             return False
-        #     else:
-        #         stack.append(i)
-    
-        # if len(stack) == 0:
-        #     return True
-        # return False
+        return len(stack) == 0 # check if stack if empty. If it is empty is means the open and closed brackets were even distributed in number
+
 
         
